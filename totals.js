2276de58cd7f77f04880068702cbf7cd34e2555f
Max Diener <mvdiener@gmail.com>
2015-06-15 22:44:18 -0700

    Added setup for multiple simultaneous ajax calls

--- totals.js
+++ totals.js
@@ -3,40 +3,59 @@
 // season 2 70040542
 // season 3 70047013
 
-// xmlhttp.onreadystatechange = function() {
-//   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//     var data = JSON.parse(xmlhttp.responseText);
-//   }
-//   return data;
-// }
-
-var xmlhttp = new XMLHttpRequest();
-var url = "http://www.netflix.com/WiMovie/70153393?trkid=13573466&actionMethod=seasonDetails&seasonId=70020061&seasonKind=ELECTRONIC";
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-var formatted = xmlhttp.responseText.replace(/\\/g,"");
-
-var div = document.createElement('div');
-div.innerHTML = formatted;
-var episodeList = div.getElementsByClassName("episodeList")[0]
-var episodeCount = episodeList.children.length;
-var episodeArray = episodeList.getElementsByClassName("mini-progress-bar");
-
-var totalPercent = seasonPercentWatched(episodeArray, episodeCount);
-
-
 // no mini-progress-bar on episodes that haven't been watched yet
 // need a way to figure out the total number of episodes without relying on progress bar
 // if an episode has been watched or has progress, there is a span with class="progress"
 
-var seasonPercentWatched = function(episodeArray, episodeCount) {
-  var totals = 0
-  for (var i = episodeArray.length - 1; i >= 0; i--) {
-    var progress = episodeArray[i].children[0].outerHTML;
-    var percentWatched = parseInt(progress.match(/\d+/)[0]);
-    totals += percentWatched
+// get jQuery loaded in console
+var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+
+
+$(function(){
+
+  // get array of season ids
+  var getSeasonIds = $("#seasonsNav").find("a").map(function(){return $(this).data("vid")});
+
+  // make ajax call to get json for each season
+  var seasonArrayJSON = [];
+
+  // set up all the ajax calls that will populate season array of json objects
+  for(var i=0; i < getSeasonIds.length; i++) {
+      $.ajax({
+          url : "http://www.netflix.com/WiMovie/70153393?trkid=13573466&actionMethod=seasonDetails&seasonId=" + getSeasonIds[i] + "&seasonKind=ELECTRONIC"
+      }).success(function(m) {
+          seasonArrayJSON.push(m);
+      });
   };
-  return (totals / episodeCount).toFixed(2);
-};
+
+  // when all ajax calls are complete, run the following function
+  $.when.apply($, seasonArrayJSON).then(processAllSeasons(seasonArrayJSON)));
+
+  // function to process all seasons
+  var processAllSeasons = function(seasonArrayJSON){
+    var percentWatchedArray = []
+
+  };
+
+  // get percent watched of a single season
+  var seasonPercentWatched = function(episodeArray, episodeCount) {
+    var totals = 0
+    for (var i = episodeArray.length - 1; i >= 0; i--) {
+      var progress = episodeArray[i].children[0].outerHTML;
+      var percentWatched = parseInt(progress.match(/\d+/)[0]);
+      totals += percentWatched
+    };
+    return (totals / episodeCount).toFixed(2);
+  };
+
+// var formatted = xmlhttp.responseText.replace(/\\/g,"");
+
+// var div = document.createElement('div');
+// div.innerHTML = formatted;
+// var episodeList = div.getElementsByClassName("episodeList")[0]
+// var episodeCount = episodeList.children.length;
+// var episodeArray = episodeList.getElementsByClassName("mini-progress-bar");
+
+// var totalPercent = seasonPercentWatched(episodeArray, episodeCount);
+
+});
