c60b2e6e2a844fbe9b6abbbe1ba868f2671bb58e
Max Diener <mvdiener@gmail.com>
2015-06-17 23:25:38 -0700

    jquery $.when not working yet, doesn't run at correct time

--- totals.js
+++ totals.js
@@ -11,56 +11,50 @@
 var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
 
 
-$(function(){
+// $(function(){
 
-  // get array of season ids
-  var getSeasonIds = $("#seasonsNav").find("a").map(function(){return $(this).data("vid")});
+// process all seasons to get percent watched for each
+var percentWatchedArray = [];
+var percentWatchedSeason = function(seasonArray){
+  $.each(seasonArray, function (index, value){
+    var episodeList = $($.parseHTML(value.html)).find(".episodeList")[0];
+    var episodeCount = episodeList.children.length;
+    var episodeArray = $(episodeList).find(".mini-progress-bar");
+    percentWatchedArray.push(seasonPercentWatched(episodeArray, episodeCount));
+    console.log(percentWatchedArray);
+  });
+};
 
-  // make ajax call to get json for each season
-  var seasonArrayJSON = [];
-
-  // set up all the ajax calls that will populate season array of json objects
-  for(var i=0; i < getSeasonIds.length; i++) {
-      $.ajax({
-          url : "http://www.netflix.com/WiMovie/70153393?trkid=13573466&actionMethod=seasonDetails&seasonId=" + getSeasonIds[i] + "&seasonKind=ELECTRONIC"
-      }).success(function(m) {
-          seasonArrayJSON.push(m);
-      });
+// get percent watched of a single season
+var seasonPercentWatched = function(episodeArray, episodeCount) {
+  var totals = 0
+  for (var i = episodeArray.length - 1; i >= 0; i--) {
+    var progress = episodeArray[i].children[0].outerHTML;
+    var percentWatched = parseInt(progress.match(/\d+/)[0]);
+    totals += percentWatched
   };
+  return (totals / episodeCount).toFixed(2);
+};
 
-  // when all ajax calls are complete, run the following function
-  $.when.apply($, seasonArrayJSON).then(processAllSeasons(seasonArrayJSON));
+// get array of season ids
+var getSeasonIds = $("#seasonsNav").find("a").map(function(){return $(this).data("vid")});
 
-  // function to process all seasons
-  var percentWatchedArray = []
-  var processAllSeasons = function(seasonArrayJSON){
-    $.each(seasonArrayJSON, function (index, value){
-      var episodeList = $($.parseHTML(value.html)).find(".episodeList")[0];
-      var episodeCount = episodeList.children.length;
-      var episodeArray = $(episodeList).find(".mini-progress-bar");
-      percentWatchedArray.push(seasonPercentWatched(episodeArray, episodeCount))
+// make ajax call to get json for each season
+var seasonArrayJSON = [];
+
+// set up all the ajax calls that will populate season array of json objects
+var seasonAjaxCalls = function(seasonIdArray){
+  for(var i=0; i < seasonIdArray.length; i++) {
+    $.ajax({
+      url : "http://www.netflix.com/WiMovie/70153393?trkid=13573466&actionMethod=seasonDetails&seasonId=" + seasonIdArray[i] + "&seasonKind=ELECTRONIC"
+    }).success(function(m) {
+      seasonArrayJSON.push(m);
     });
-  };
+  }
+  return seasonArrayJSON;
+};
 
-  // get percent watched of a single season
-  var seasonPercentWatched = function(episodeArray, episodeCount) {
-    var totals = 0
-    for (var i = episodeArray.length - 1; i >= 0; i--) {
-      var progress = episodeArray[i].children[0].outerHTML;
-      var percentWatched = parseInt(progress.match(/\d+/)[0]);
-      totals += percentWatched
-    };
-    return (totals / episodeCount).toFixed(2);
-  };
+// when all ajax calls are complete, run the following function
+$.when.apply($, seasonAjaxCalls(getSeasonIds)).then(percentWatchedSeason(seasonArrayJSON));
 
-// var formatted = xmlhttp.responseText.replace(/\\/g,"");
-
-// var div = document.createElement('div');
-// div.innerHTML = formatted;
-// var episodeList = div.getElementsByClassName("episodeList")[0]
-// var episodeCount = episodeList.children.length;
-// var episodeArray = episodeList.getElementsByClassName("mini-progress-bar");
-
-// var totalPercent = seasonPercentWatched(episodeArray, episodeCount);
-
-});
+// });
