c14ce52105ac4e71e6dbbb07d1008801b5f97fe6
Maximus Prime <mvdiener@gmail.com>
2017-03-12 22:14:02 -0700

    Update totals.js

--- totals.js
+++ totals.js
@@ -1,60 +1,54 @@
+function fillArray() {
+	var array = [];
+	// Uncomment below to test with sample size that is random modulo 3
+	// var amount = Math.floor(Math.random() * (2 - 0 + 1)) + 0
+	while (array.length < 1000 /*+ amount*/) {
+		var rand = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+		array.push(rand);
+	}
+	return array;
+}
 
-// season 1 70020061
-// season 2 70040542
-// season 3 70047013
+function removePieces(array) {
+	while(array.length >= 3) {
+		var pickThree = [array.pop(), array.pop(), array.pop()];
+		pickThree.sort();
+		if (pickThree[0] != pickThree[2] && (pickThree[1] == pickThree[2] || pickThree[1] == pickThree[0])) {
+			if (pickThree[1] == pickThree[0]) {
+				array.push(pickThree[2]);
+			} else {
+				array.push(pickThree[0]);
+			}
+		}
+		shuffleArray(array);
+	}
+	return array;
+}
 
-// no mini-progress-bar on episodes that haven't been watched yet
-// need a way to figure out the total number of episodes without relying on progress bar
-// if an episode has been watched or has progress, there is a span with class="progress"
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
 
-// get jQuery loaded in console
-var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+function run() {
+	var bag = fillArray();
+	var eaten = removePieces(bag);
+	return (eaten);
+}
 
-
-// $(function(){
-
-// process all seasons to get percent watched for each
-var percentWatchedArray = [];
-var percentWatchedSeason = function(seasonArray){
-  $.each(seasonArray, function (index, value){
-    var episodeList = $($.parseHTML(value.html)).find(".episodeList")[0];
-    var episodeCount = episodeList.children.length;
-    var episodeArray = $(episodeList).find(".mini-progress-bar");
-    percentWatchedArray.push(seasonPercentWatched(episodeArray, episodeCount));
-    console.log(percentWatchedArray);
-  });
-};
-
-// get percent watched of a single season
-var seasonPercentWatched = function(episodeArray, episodeCount) {
-  var totals = 0
-  for (var i = episodeArray.length - 1; i >= 0; i--) {
-    var progress = episodeArray[i].children[0].outerHTML;
-    var percentWatched = parseInt(progress.match(/\d+/)[0]);
-    totals += percentWatched
-  };
-  return (totals / episodeCount).toFixed(2);
-};
-
-// get array of season ids
-var getSeasonIds = $("#seasonsNav").find("a").map(function(){return $(this).data("vid")});
-
-// make ajax call to get json for each season
-var seasonArrayJSON = [];
-
-// set up all the ajax calls that will populate season array of json objects
-var seasonAjaxCalls = function(seasonIdArray){
-  for(var i=0; i < seasonIdArray.length; i++) {
-    $.ajax({
-      url : "http://www.netflix.com/WiMovie/70153393?trkid=13573466&actionMethod=seasonDetails&seasonId=" + seasonIdArray[i] + "&seasonKind=ELECTRONIC"
-    }).success(function(m) {
-      seasonArrayJSON.push(m);
-    });
-  }
-  return seasonArrayJSON;
-};
-
-// when all ajax calls are complete, run the following function
-$.when.apply($, seasonAjaxCalls(getSeasonIds)).then(percentWatchedSeason(seasonArrayJSON));
-
-// });
+function runPercentages(timesToRun) {
+	var obj = {0: 0, 1: 0, 2: 0}
+	for (var i = 0; i <= timesToRun; i++) {
+		var eaten = run();
+		obj[eaten.length] += 1;
+	}
+	total = obj[0] + obj[1] + obj[2];
+	console.log((obj[2]/total)*100 + "% chance to have two pieces remaining.");
+	console.log((obj[1]/total)*100 + "% chance to have one piece remaining.");
+	console.log((obj[0]/total)*100 + "% chance to have no pieces remaining.");
+}
